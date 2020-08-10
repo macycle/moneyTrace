fc4e8b98a443f33013e3910ce451dc707fa737d7
macycle <1124942097@qq.com>
2020-08-10 22:27:39 +0800

    添加了自动生成id的函数

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import {defaultExpenseTags} from '@/contants/defaultTags';
-
+import idCreator from '@/lib/idCreator';
 Vue.use(Vuex);
 
 
@@ -29,7 +29,59 @@ const store=new Vuex.Store({
 
     saveTag(state) {
       window.localStorage.setItem('tag-list',JSON.stringify(state.tagList))
-    }  
+    },
+    
+    /*record-list相关操作*/
+    insertRecord (state,record: RecordItem){
+      record.id=idCreator();
+      record.createAt=new Date();
+      record.amount=Math.abs(record.amount);
+      state.recordList.push(record);
+      store.commit('saveRecord');
+    },
+
+    findRecord(state,id: number){
+      state.currentRecord=undefined;
+      const record=state.recordList.filter(item=>item.id===id)[0];
+      if(record){
+        state.currentRecord=record;
+      }
+    },
+
+    updateRecord(state,payload: {id: number;record: RecordItem}){
+      const {id,record}=payload;    //析构赋值；
+         //TODO
+      for( let index=0;index<state.recordList.length;index++){
+        if(state.recordList[index].id===id){
+          state.recordList[index]=record;
+          break;
+        }
+      }
+
+      store.commit('saveRecord');
+    },
+
+    removeRecord(state,id: number){
+      state.recordListError='';
+      let i;
+      for (i=0;i<state.recordList.length;i++){
+        if(state.recordList[i].id===id){
+          break;
+        }
+      }
+
+      if (i === state.recordList.length) {
+        state.recordListError = 'notfound';
+    } else {
+        state.recordList.splice(i, 1);
+        store.commit('saveRecord');
+    }
+    },
+
+    saveRecord(state){
+      window.localStorage.setItem('record-list',JSON.stringify(state.recordList))
+    },
+    
     
   }
 })
