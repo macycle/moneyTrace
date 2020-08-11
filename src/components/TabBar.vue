90abed6df722d6d9600ac87f6385259457e59576
macycle <1124942097@qq.com>
2020-08-11 10:17:32 +0800

    完成tabbar组件的封装与clone深拷贝

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,9 +1,9 @@
 <template>
     <ul class="tab-bar" :class="{[classPrefix+'-tab-bar']:classPrefix}">
         <li class="tab-bar-item" :class="{[classPrefix+'-tab-bar-item']:classPrefix,'select':cBar===bar.value}"
-        v-for="(bar,index) in bars" :key="index" @click="select(bar.value)"
+        v-for="(bar,index) in bars" :key="index" @click="toggle(bar.value)"
         >
-            我是tabbar
+            {{bar.name}}
         </li>
     </ul>
 </template>
@@ -20,30 +20,32 @@ type TabBarItem = {
 @Component
     export default class TabBar extends Vue {
         @Prop(String) classPrefix?: string;
-        @Prop({required: true,type: Array})bars!: TabBarItem[];
-        @Prop({required: true,type: String})cBar!: string;
+        @Prop({required: true,type: Array}) bars!: TabBarItem[];
+        @Prop({required: true,type: String}) cBar!: string;
 
 
-        select(barValue: string){
+        toggle(barValue: string){
             this.$emit('update:cBar',barValue)
         }
     }
 </script>
 
 <style lang="scss" scoped>
-
 .tab-bar{
     display: flex;
     justify-content: center;
     align-items: center;
 
-    &-item{
+    .tab-bar-item{
         font-size: 20px;
         padding: 4px;
         border-bottom: 1px solid transparent;
-
+       
+        
+       
         &.selected{
             border-bottom: 1px solid #333333;
+           
         }
     }
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,25 +1,66 @@
 <template>
     <div class="money">
        <div class='types'>
-            <TabBar class-prefix="types" :bars="[{name: '支出', value: '-'}, {name: '收入', value: '+'}]" :c-bar.sync="record.type"/>
+            <TabBar class-prefix="types" :bars="barsValue" :c-bar.sync="record.type"/>
+            <button class="cancel" @click="cancel">取消</button>
        </div>
+
+       <TagList></TagList>
     </div>
 </template>
 
-<script>
+<script lang='ts'>
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import TabBar from '@/components/TabBar.vue';
+import clone from '@/lib/clone';
+
 @Component({
     components:{TabBar}
 })
     export default class Money extends Vue{
-        
+        barsValue=[{name: '支出', value: '-'}, {name: '收入', value: '+'}];
+        record: RecordItem =this.initRecord();
+
+        get tagList(): TagItem[]{
+            return this.$store.state.tagList;
+        }
+
+        initRecord(){
+            return {tag: {name: 'food', value: '餐饮'}, 
+                    type: '-',
+                    note: '',
+                    amount: 0};  //直接赋值不用声明；
+        }
+
+        cancel(){
+            this.$router.replace('/bill');
+        }
+
+        complete(){
+            this.$store.commit('insertRecord',clone<RecordItem>(this.record));
+            this.record=this.initRecord();
+            this.$router.replace('/bill')
+            }
 
     }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/styles/style.scss";
+
+.types{
+    background:#7C48DB;
+    display:flex;
+    justify-content: center;
+    position: relative;
+
+    ::v-deep .types-tab-bar-item {
+            padding: 24px 16px 8px 16px;
+        }
+
+}
+
+
 
 </style>
