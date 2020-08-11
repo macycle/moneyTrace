a34da9169ffe39aa60ce924ced9af3c520cb7600
macycle <1124942097@qq.com>
2020-08-11 16:15:31 +0800

    新增bill页面

---
+++ src/views/Bill.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+import {Component, Watch} from 'vue-property-decorator';
+
+@Component
+    export default class Bill extends Vue {
+        
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
