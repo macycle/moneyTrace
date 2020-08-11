82add259104d7d379ace8dd134f40426cfac1343
macycle <1124942097@qq.com>
2020-08-11 10:51:58 +0800

    完成icon组件的配置，能够自动导入icon图片

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -8,6 +8,8 @@
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
 
+const importAll=(requireContext: __WebpackModuleApi.RequireContext)=>requireContext.keys().forEach(requireContext);
+ try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 @Component
     export default class Icon extends Vue {
@@ -16,5 +18,13 @@ import {Component, Prop} from 'vue-property-decorator';
 </script>
 
 <style lang="scss" scoped>
+
+    svg{
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.15em;
+        overflow: hidden;
+        fill:currentColor;
+    }
 
 </style>
