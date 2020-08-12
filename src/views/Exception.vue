742e43519121a41f3dcb7e6f6ca92b360e8f1a23
macycle <1124942097@qq.com>
2020-08-12 10:21:12 +0800

    新增404页面

---
+++ src/views/Exception.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        404
+    </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+    export default class Exception extends Vue {
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
